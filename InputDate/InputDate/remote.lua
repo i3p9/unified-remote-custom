9c81e9ba8f2c157dedd6aafe5d9b702ca3371930
Hackerman <sphincone@gmail.com>
2020-09-18 21:38:15 -0400

    Initial remote upload

---
+++ InputDate/InputDate/remote.lua
@@ -0,0 +1,233 @@
+local kb = libs.keyboard;
+
+
+-- Documentation
+-- http://www.unifiedremote.com/api
+
+-- Keyboard Library
+-- http://www.unifiedremote.com/api/libs/keyboard
+
+
+--@help Command 1
+actions.year1 = function ()
+	kb.stroke("d2","d0","d1","d4");
+end
+
+
+--@help Command 2
+actions.year2 = function ()
+	kb.stroke("d2","d0","d1","d5");
+end
+
+actions.month1 = function ()
+    kb.stroke("d0","d1");
+end
+
+
+actions.month2 = function ()
+    kb.stroke("d0","d2");
+end
+
+
+actions.month3 = function ()
+    kb.stroke("d0","d3");
+end
+
+
+actions.month4 = function ()
+    kb.stroke("d0","d4");
+end
+
+
+actions.month5 = function ()
+    kb.stroke("d0","d5");
+end
+
+
+actions.month6 = function ()
+    kb.stroke("d0","d6");
+end
+
+
+actions.month7 = function ()
+    kb.stroke("d0","d7");
+end
+
+
+actions.month8 = function ()
+    kb.stroke("d0","d8");
+end
+
+
+actions.month9 = function ()
+    kb.stroke("d0","d9");
+end
+
+
+actions.month10 = function ()
+    kb.stroke("d1","d0");
+end
+
+
+actions.month11 = function ()
+    kb.stroke("d1","d1");
+end
+
+
+actions.month12 = function ()
+    kb.stroke("d1","d2");
+end
+
+actions.day1 = function ()
+    kb.stroke("d0","d1");
+end
+
+
+actions.day2 = function ()
+    kb.stroke("d0","d2");
+end
+
+
+actions.day3 = function ()
+    kb.stroke("d0","d3");
+end
+
+
+actions.day4 = function ()
+    kb.stroke("d0","d4");
+end
+
+
+actions.day5 = function ()
+    kb.stroke("d0","d5");
+end
+
+
+actions.day6 = function ()
+    kb.stroke("d0","d6");
+end
+
+
+actions.day7 = function ()
+    kb.stroke("d0","d7");
+end
+
+
+actions.day8 = function ()
+    kb.stroke("d0","d8");
+end
+
+
+actions.day9 = function ()
+    kb.stroke("d0","d9");
+end
+
+
+actions.day10 = function ()
+    kb.stroke("d1","d0");
+end
+
+
+actions.day11 = function ()
+    kb.stroke("d1","d1");
+end
+
+
+actions.day12 = function ()
+    kb.stroke("d1","d2");
+end
+
+
+actions.day13 = function ()
+    kb.stroke("d1","d3");
+end
+
+
+actions.day14 = function ()
+    kb.stroke("d1","d4");
+end
+
+
+actions.day15 = function ()
+    kb.stroke("d1","d5");
+end
+
+
+actions.day16 = function ()
+    kb.stroke("d1","d6");
+end
+
+
+actions.day17 = function ()
+    kb.stroke("d1","d7");
+end
+
+
+actions.day18 = function ()
+    kb.stroke("d1","d8");
+end
+
+
+actions.day19 = function ()
+    kb.stroke("d1","d9");
+end
+
+
+actions.day20 = function ()
+    kb.stroke("d2","d0");
+end
+
+
+actions.day21 = function ()
+    kb.stroke("d2","d1");
+end
+
+
+actions.day22 = function ()
+    kb.stroke("d2","d2");
+end
+
+
+actions.day23 = function ()
+    kb.stroke("d2","d3");
+end
+
+
+actions.day24 = function ()
+    kb.stroke("d2","d4");
+end
+
+
+actions.day25 = function ()
+    kb.stroke("d2","d5");
+end
+
+
+actions.day26 = function ()
+    kb.stroke("d2","d6");
+end
+
+
+actions.day27 = function ()
+    kb.stroke("d2","d7");
+end
+
+
+actions.day28 = function ()
+    kb.stroke("d2","d8");
+end
+
+
+actions.day29 = function ()
+    kb.stroke("d2","d9");
+end
+
+
+actions.day30 = function ()
+    kb.stroke("d3","d0");
+end
+
+
+actions.day31 = function ()
+    kb.stroke("d3","d1");
+end
